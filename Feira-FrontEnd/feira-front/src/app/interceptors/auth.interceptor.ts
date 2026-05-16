f6ba0d9e151024114ca65ac777f795a2a364510a
Leanderson Lima <Leandersonlima137@gmail.com>
2026-05-15 22:58:36 -0300

    Ajustando interceptor para login

--- Feira-FrontEnd/feira-front/src/app/interceptors/auth.interceptor.ts
+++ Feira-FrontEnd/feira-front/src/app/interceptors/auth.interceptor.ts
@@ -2,6 +2,10 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
+  if (req.url.includes('/auth/')) {
+    return next(req);
+  }
+
   const token = localStorage.getItem('token');
 
   if (token) {
