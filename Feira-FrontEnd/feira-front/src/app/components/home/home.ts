ce54dc88349a8410fbd064c2591b5476ed3da576
Leanderson Lima <Leandersonlima137@gmail.com>
2026-05-21 13:41:34 -0300

    Melhorando visual da home dashboard

--- Feira-FrontEnd/feira-front/src/app/components/home/home.ts
+++ Feira-FrontEnd/feira-front/src/app/components/home/home.ts
@@ -1,13 +1,103 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { finalize } from 'rxjs';
+
+import { Categoria } from '../../models/categoria';
+import { Feirante } from '../../models/feirante';
+
+import { CategoriaService } from '../../services/categoria.service';
+import { FeiranteService } from '../../services/feirante.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  categorias: Categoria[] = [];
+  feirantes: Feirante[] = [];
+
+  carregandoCategorias = false;
+  carregandoFeirantes = false;
+
+  mensagem = '';
+
+  constructor(
+    private categoriaService: CategoriaService,
+    private feiranteService: FeiranteService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this.carregarDados();
+  }
+
+  carregarDados(): void {
+    this.listarCategorias();
+    this.listarFeirantes();
+  }
+
+  listarCategorias(): void {
+    this.carregandoCategorias = true;
+
+    this.categoriaService.listar()
+      .pipe(
+        finalize(() => {
+          this.carregandoCategorias = false;
+          this.cdr.detectChanges();
+        })
+      )
+      .subscribe({
+        next: (dados) => {
+          this.categorias = dados;
+        },
+        error: () => {
+          this.mensagem = 'Erro ao carregar categorias.';
+        }
+      });
+  }
+
+  listarFeirantes(): void {
+    this.carregandoFeirantes = true;
+
+    this.feiranteService.listar()
+      .pipe(
+        finalize(() => {
+          this.carregandoFeirantes = false;
+          this.cdr.detectChanges();
+        })
+      )
+      .subscribe({
+        next: (dados) => {
+          this.feirantes = dados;
+        },
+        error: () => {
+          this.mensagem = 'Erro ao carregar feirantes.';
+        }
+      });
+  }
+
+  totalCategorias(): number {
+    return this.categorias.length;
+  }
+
+  totalFeirantes(): number {
+    return this.feirantes.length;
+  }
+
+  totalAtivos(): number {
+    return this.feirantes.filter(feirante => feirante.ativo).length;
+  }
+
+  totalInativos(): number {
+    return this.feirantes.filter(feirante => !feirante.ativo).length;
+  }
+
+  carregando(): boolean {
+    return this.carregandoCategorias || this.carregandoFeirantes;
+  }
 }
